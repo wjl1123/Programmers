77fc851cec512dfca8481e2624de47ad75cee136
wjl1123 <lee.woojae2@gmail.com>
2021-04-05 23:57:22 +0900

    committed

--- stocks.py
+++ stocks.py
@@ -80,3 +80,21 @@ def solution(prices):
                 answer[i] += 1
                 break
     return answer
+
+from collections import deque
+def solution(prices):
+    answer = []
+    prices = deque(prices)
+    while prices:
+        c = prices.popleft()
+
+        count = 0
+        for i in prices:
+            if c > i:
+                count += 1
+                break
+            count += 1
+
+        answer.append(count)
+
+    return answer
