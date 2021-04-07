37975a2118544660715ce230b745e4a0ccec9421
wjl1123 <lee.woojae2@gmail.com>
2021-04-07 21:51:28 +0900

    committed

--- func_dev.py
+++ func_dev.py
@@ -102,4 +102,22 @@ def solution(progresses, speeds):
 
     return answer
 
-solution(progresses, speeds)
+solution(progresses3, speeds3)
+
+def solution1(progresses, speeds):
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0] < -((p-100)//s):
+            Q.append([-((p-100)//s),1])
+            print('if')
+            print(Q)
+        else:
+            Q[-1][1]+=1
+            print('else')
+            print(Q)
+
+    print(Q)
+
+    return [q[1] for q in Q]
+
+solution1(progresses3, speeds3)
